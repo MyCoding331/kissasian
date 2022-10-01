a41964553f02a33584fff65be5ad8fad2108da7c
MyCoding331 <100577359+MyCoding331@users.noreply.github.com>
2022-10-01 12:38:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,27 +6,15 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
-app.set("trust proxy", 1);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); 
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE"); 
-  res.setHeader( 
-    "Access-Control-Allow-Methods", 
-    "Content-Type",
-    "Authorization"
-  );
-  next();
-});
-const baseURL = "https://mykissasian.com/";
-// const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`;
-// const airing = "https://animebee.to/top-airing-anime";
-// const tv = `https://dramanice.ac/drama/crazy-love--detail`;
-// const single = "https://animebee.to/";
+
+const baseURL = "https://kissasian.fan/";
 
 app.get("/", (req, res) => {
   let info = {
-    popular: `http://localhost:${PORT}/api/popular/:page`,
-    movies: `http://localhost:${PORT}/api/movies/:id`,
+    popular: `http://localhost:${PORT}/api/popular/page=:page`,
+    movies: `http://localhost:${PORT}/api/movies/page=:page`,
+    recentlyAadded: `http://localhost:${PORT}/api/recently-added/drama/page=:page`,
+    kshow: `http://localhost:${PORT}/api/kshow/page=:page`,
     search: `http://localhost:${PORT}/api/search/:word/:page`,
     episode_link: `http://localhost:${PORT}/api/watching/:id`,
 
@@ -35,204 +23,319 @@ app.get("/", (req, res) => {
   res.send(info);
 });
 
-app.get("/api/movies/page=:page", (req, res) => {
-  const popular = `${baseURL}category/movies?country=&page=${req.params.page}`;
-  // const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`
+app.get("/api/popular/page=:page", (req, res) => {
+  const popular = `${baseURL}trending//page/${req.params.page}`;
+
   axios(popular)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
       const popular = [];
 
-      $(".item-lists >.col-6").each(function (index, element) {
-        const title = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > .text-h4 "
-          )
-          .text();
+      $(".box > li").each(function (index, element) {
+        const title = $(this).find(".mask ").attr().title;
+        const id = $(this).find(" .mask ").attr().href.slice(28);
+        const image = $(this).find(" .mask > img").attr("src");
+        const episodes = $(this).find(" .mask > .ep").text();
+        const  time = $(this).find(" .mask > .time").text();
 
-        const id = $(this).find(" a ").attr().href.slice(7);
-        const image = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > img  "
-          )
-          .attr("src");
-        // const date = $(this).find(".bottom > .country").text();
-        popular.push({ title, id, image });
+        popular.push({ title, id, image,episodes ,time});
       });
       res.json(popular);
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/api/recentlyadded/page=:page", (req, res) => {
+  const drama = `${baseURL}dramas/page/${req.params.page}`;
+
+  axios(drama)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const drama = [];
+
+      $(".box > li").each(function (index, element) {
+        const title = $(this).find(".mask ").attr().title;
+        const id = $(this).find(" .mask ").attr().href.slice(28);
+        const image = $(this).find(" .mask > img").attr("src");
+        const episodes = $(this).find(" .mask > .ep").text();
+        const  time = $(this).find(" .mask > .time").text();
+        drama.push({ title, id, image, time, episodes });
+      });
+      res.json(drama);
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/api/movies/page=:page", (req, res) => {
+  const movies = `${baseURL}movies/page/${req.params.page}`;
+
+  axios(movies)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const movies = [];
+
+      $(".box > li").each(function (index, element) {
+        const title = $(this).find(".mask ").attr().title;
+        const id = $(this).find(" .mask ").attr().href.slice(28);
+        const image = $(this).find(" .mask > img").attr("src");
+        const episodes = $(this).find(" .mask > .ep").text();
+        const  time = $(this).find(" .mask > .time").text();
+        movies.push({ title, id, image, time, episodes });
+      });
+      res.json(movies);
     })
     .catch((err) => console.log(err));
 });
 app.get("/api/kshow/page=:page", (req, res) => {
-  const popular = `${baseURL}category/k-show?country=&page=${req.params.page}`;
-  // const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`
-  axios(popular)
+  const kshow = `${baseURL}recently-added-kshow?page=${req.params.page}`;
+
+  axios(kshow)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-      const popular = [];
+      const kshow = [];
 
-      $(".item-lists >.col-6").each(function (index, element) {
-        const title = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > .text-h4 "
-          )
-          .text();
+      $(".list-episode-item >li").each(function (index, element) {
+        const title = $(this).find("a").attr().title;
+        const id = $(this).find(" a").attr().href.slice(33);
+        const image = $(this).find(" a > img").attr("data-original");
+        const time = $(this).find(" a > .time").text();
+        const type = $(this).find(" a > .type").text();
+        const ep = $(this).find(" a > .ep").text();
 
-        const id = $(this).find(" a ").attr().href.slice(7);
-        const image = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > img  "
-          )
-          .attr("src");
-
-        popular.push({ title, id, image });
+        kshow.push({ title, id, image, time, type, ep });
       });
-      res.json(popular);
+      res.json(kshow);
     })
     .catch((err) => console.log(err));
 });
-app.get("/api/ongoing-drama/page=:page", (req, res) => {
-  const popular = `${baseURL}category/drama?country=&page=${req.params.page}`;
-  // const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`
-  axios(popular)
+
+app.get("/api/drama-detail-genres/:id", (req, res) => {
+  const detail = `${baseURL}${req.params.id}`;
+  axios(detail)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-      const popular = [];
+      const detail = [];
+      // const episodes = [];
 
-      $(".item-lists >.col-6").each(function (index, element) {
-        const title = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > .text-h4 "
-          )
+      $(".content-left > .block > .details > .info > p ").each(function (
+        index
+      ) {
+        const genres = $(".content-left > .block > .details > .info > p ")
+          .last()
+          .find("a")
+
           .text();
+        const id = $(".content-left > .block > .details > .info > p ")
+          .last()
+          .find("a")
 
-        const id = $(this).find(" a ").attr().href.slice(7);
-        const image = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > img  "
-          )
-          .attr("src");
+          .attr("href");
 
-        popular.push({ title, id, image });
+        detail.push({
+          genres,
+          id,
+        });
       });
-      res.json(popular);
-    })
-    .catch((err) => console.log(err));
-});
-app.get("/api/recently-added/page=:page", (req, res) => {
-  const popular = `${baseURL}drama/${req.params.page}`;
-  // const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`
-  axios(popular)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const popular = [];
-
-      $(".item-lists >.col-6").each(function (index, element) {
-        const title = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > .text-h4 "
-          )
-          .text();
-        const time = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > .status-type > .text-h6 "
-          )
-          .text();
-
-        const episodeNo = $(this)
-          .find("a > .episode-card > .episode-ratio  > .episode-number ")
-          .text();
-        const id = $(this).find(" a ").attr().href.slice(7);
-        const image = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > img  "
-          )
-          .attr("src");
-        // const date = $(this).find(".bottom > .country").text();
-        popular.push({ title, id, image, episodeNo, time });
-      });
-      res.json(popular);
-    })
-    .catch((err) => console.log(err));
-});
-app.get("/api/upcoming/", (req, res) => {
-  const popular = `${baseURL}upcoming/`;
-  // const popular = `https://dramanice.ac/most-popular-drama?page=${req.params.page}`
-  axios(popular)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const popular = [];
-
-      $(".item-lists >.col-6").each(function (index, element) {
-        const title = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > .text-h4 "
-          )
-          .text();
-
-        const id = $(this).find(" a ").attr().href.slice(7);
-        const image = $(this)
-          .find(
-            "a > .episode-card > .episode-ratio  > .episode-detail  > div > img  "
-          )
-          .attr("src");
-        // const date = $(this).find(".bottom > .country").text();
-        popular.push({ title, id, image });
-      });
-      res.json(popular);
+      res.json(detail);
     })
     .catch((err) => console.log(err));
 });
 app.get("/api/drama-detail/:id", (req, res) => {
-  const tv = `${baseURL}detail/${req.params.id}`;
-  axios(tv)
+  const detail = `${baseURL}Drama/${req.params.id}`;
+  axios(detail)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-      const tv = [];
+      const detail = [];
+      // const episodes = [];
 
-      $(".single  > .row").each(function (index) {
-        const img = $(this)
-          .find(".col-12 >.row > .col-6 > .episode-ratio > img")
-          .attr("src");
-        const title = $(this)
-          .find(".col-rightx > .box > .box-header >.film-title > a")
-          .text();
+      $(".space-top ").each(function (index) {
+        const image = $(this).find(".section > .cover > img").attr("src");
+        const title = $(this).find(".gold >.heading > h3 ").text();
+
+        const description = $(this)
+          .find(".section > .info > .summary1")
+
+          .text()
+          .slice(21, -17);
+        // const genres = $(this)
+        //   .find(".block > .details > .info > p ")
+        //   .last()
+        //   .children("a")
+        //   .attr();
+
+        const releaseYear = $(this)
+          .find(".section > .info  > p ")
+          .first()
+
+          .next()
+
+          .text()
+          .slice(32, -17);
 
         const status = $(this)
-          .find(
-            ".row > .col-12 > .series-details > col-12 > .status"
-          )
-          .text();
-        const year = $(this)
-          .find(
-            ".row > .col-12 > .series-details > col-12 > .block > .text-light"
-          )
-          .text();
-        const country = $(this)
-          .find(
-            ".row > .col-12 > .series-details > col-12 > .block > .text-light"
-          )
-          .text();
-        const description = $(this)
-          .find(".box-body >.row > #show-detailsxx > .show-synopsis > p > span")
-          .text();
+          .find(".section > .info  > p ")
+          .first()
 
-        tv[index]= ({
+          .next()
+          .next()
+
+          .text()
+          .slice(29, -17);
+        const country = $(this)
+          .find(".section > .info  > a")
+
+          .text();
+        const views = $(this)
+          .find(".section > .info  > p ")
+          .first()
+
+          .next()
+          .next()
+          .next()
+
+          .text()
+          .slice(29, -17);
+
+        detail.push({
           title,
-          img,
-          status,
-          year,
-          country,
+          image,
           description,
-        }); //<-- cannot be a function expression
+          // genres,
+          releaseYear,
+          status,
+          country,
+          views,
+        });
       });
-      res.json(tv);
+      res.json(detail);
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/api/drama-detail-cast/:id", (req, res) => {
+  const detailCast = `${baseURL}Drama/${req.params.id}`;
+  axios(detailCast)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const detailCast = [];
+      // const episodes = [];
+
+      $(".list-actor > .item").each(function (index) {
+        const image = $(this).find(".actor-ava").attr("style").slice(21, -1);
+        const castName = $(this).find(".actor-info > a").text();
+
+        const id = $(this).find(".actor-ava").attr("href").slice(7);
+
+        detailCast[index] = {
+          castName,
+          image,
+          id,
+        };
+      });
+      res.json(detailCast);
+    })
+    .catch((err) => console.log("Fuck off"));
+});
+
+app.get("/api/drama-episodes/:id", (req, res) => {
+  const episodes = `${baseURL}Drama/${req.params.id}`;
+  axios(episodes)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const episodes = [];
+
+      $(".list > .episodeSub ").each(function (index) {
+        const id = $(this).children("a").attr("href").slice(7);
+        const episode = $(this).find("a > span").text();
+
+        episodes.push({
+          id,
+
+          episode,
+        });
+
+        //<-- cannot be a function expression
+      });
+      res.json(episodes);
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/api/drama-watch-servers/:id", (req, res) => {
+  const watching = `${baseURL}${req.params.id}`;
+  axios(watching)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const servers = [];
+
+      $(".anime_muti_link > ul > li ").each(function (index, i) {
+        const title = $(this).children().remove().end().text();
+        const frame = $(this).attr("data-video");
+        // const Download = $(this).find(".plugins2  ").children(".download").children("a").attr("href");
+
+        servers[index] = { title, frame };
+
+        //<-- cannot be a function expression
+      });
+
+      res.json({ servers });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/api/drama-watch/", (req, res) => {
+  const moviewatch = `https://keephealth.info/p/fda-approved-weight-loss-pills-over-the-counter?sig=U2NhcmxldC1IZWFydC1SeWVvfHx8RXBpc29kZS0xfHx8MTY2NDM2Mzk1OQ==&id=30070&op=swa`;
+
+  axios(moviewatch)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const watch = [];
+
+      $(".main > .wrap").each(function (index, i) {
+        const title = $(this)
+          .find(".space-top > .gold > .heading > h3 ")
+          .text();
+        const frame = $(this)
+          .find(
+            ".space-top > .video-container > #mVideo > #document > html > body > #vstr > .jw-wrapper > .jw-media  > .jw-video "
+          )
+          .attr("src");
+        // const Download = $(this).find(".plugins2  ").children(".download").children("a").attr("href");
+
+        watch[index] = { title, frame };
+
+        //<-- cannot be a function expression
+      });
+
+      res.json({ watch });
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/api/search/keyword=:word/page=:page", (req, res) => {
+  const search = `${baseURL}page/${req.params.page}?type=movies&s=${req.params.word}`;
+
+  axios(search)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const search = [];
+
+      $(".list-episode-item >li").each(function (index, element) {
+        const title = $(this).find("a").attr().title;
+        const id = $(this).find(" a").attr().href.slice(28);
+        const image = $(this).find(" a > img").attr("data-original");
+        const type = $(this).find(" a > .type").text();
+
+        search.push({ title, id, image, type });
+      });
+      res.json(search);
     })
     .catch((err) => console.log(err));
 });
